7681e179a39c5444df3a8d6ca2cdd1f2326645d4
Matthew Clegg <matthewcleggphd@gmail.com>
2016-01-07 19:46:09 -0500

    Update support.R

--- R/support.R
+++ R/support.R
@@ -1,5 +1,20 @@
 # support.R -- a miscellaneous collection of supporting functions
 
+# Copyright (C) 2016 Matthew Clegg
+
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  A copy of the GNU General Public License is available at
+#  http://www.r-project.org/Licenses/
+
 ctext <- function (str, n) {
     # Centers the string str in a field of n blanks
     if (nchar(str) == n) return(str)
