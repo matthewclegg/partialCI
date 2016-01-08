a0dd64bdcbca9f8b94f22c1682f2b144cec8ec52
Matthew Clegg <matthewcleggphd@gmail.com>
2016-01-07 19:45:53 -0500

    Update rand.R

--- R/rand.R
+++ R/rand.R
@@ -1,5 +1,20 @@
 # rand.R -- functions for generating random variates
 
+# Copyright (C) 2016 Matthew Clegg
+
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  A copy of the GNU General Public License is available at
+#  http://www.r-project.org/Licenses/
+
 
 rpci <- function (n, alpha, beta, sigma_C, rho, sigma_M, sigma_R, 
                   include.state = FALSE, robust=FALSE, nu=5) {
